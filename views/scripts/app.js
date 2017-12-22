8c0eeb35528e60f54f81caf661eed37578638057
Lars Huring <lars.huring@gmail.com>
2017-12-22 13:56:42 +0100

    Fixed upload field

--- views/scripts/app.js
+++ views/scripts/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    console.log('hello world');
+    $(':file').on('fileselect', function(event, numFiles, label) {
+        console.log(numFiles);
+        console.log(label);
+    });
 });
 
 $('.stat-card').on('click', function(evt) {
@@ -8,4 +11,11 @@ $('.stat-card').on('click', function(evt) {
         console.log(data);
     });
 
+});
+
+$(document).on('change', ':file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
 });
