d5c0abf1251c66511e0f1216753b9394debd3495
Lars Huring <lars.huring@gmail.com>
2018-01-17 09:24:27 +0100

    Refactoring

--- routes/sessions.js
+++ routes/sessions.js
@@ -2,7 +2,6 @@ const hbs = require('express-hbs')
 
 var { Sessions } = require('../models/sessions');
 var { HuntingSession } = require('../models/classes/huntingSession.class');
-var { User } = require('../models/user');
 var { Weapon } = require('../models/weapon');
 
 var formattingHelper = require("../models/helpers/helper.formatting");
