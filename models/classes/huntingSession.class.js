0f70b7c6a9c94dd30ab1ec6157fb77e119776105
Lars Huring <lars.huring@gmail.com>
2018-01-17 09:28:15 +0100

    Removed getter/setters
    Couldn't get a clean object, so removed overhead

--- models/classes/huntingSession.class.js
+++ models/classes/huntingSession.class.js
@@ -3,34 +3,19 @@ class HuntingSession {
     constructor(obj) {
 
         if (obj) {
-            this['_id'] = obj._id;
-            this['_uid'] = obj.uid;
-            this['_location'] = obj.location;
-            this['_duration'] = obj.duration;
-            this['_distance'] = obj.distance;
-            this['_kills'] = obj.kills;
-            this['_commentText'] = obj.commentText;
-            this['_timestamp'] = obj.timestamp;
-            this['_file'] = obj.file;
-            this['_animalType'] = obj.animalType;
-            this['_weapon'] = obj.weapon;
+            this.id = obj._id;
+            this.uid = obj.uid;
+            this.location = obj.location;
+            this.duration = obj.duration;
+            this.distance = obj.distance;
+            this.kills = obj.kills;
+            this.commentText = obj.commentText;
+            this.timestamp = obj.timestamp;
+            this.file = obj.file;
+            this.animalType = obj.animalType;
+            this.weapon = obj.weapon;
         }
     }
-
-    // Public variables
-    get id() { return this['_id']; }
-    get uid() { return this['_uid']; }
-    get location() { return this['_location']; }
-    get duration() { return this['_duration']; }
-    get distance() { return this['_distance']; }
-    get kills() { return this['_kills']; }
-    get comment() { return this['_commentText']; }
-    get timestamp() { return this['_timestamp']; }
-    get image() { return this['_file']; }
-    get animalType() { return this['_animalType']; }
-    
-    get weapon() { return this['_weapon']; }
-    set weapon(weapon) { this['_weapon'] = weapon; }
 
     add() {
         return new Promise((resolve, reject) => {
